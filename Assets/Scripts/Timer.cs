20a8cd6b5eb8ee19b0ff11de59fa04fac7ba5d64
Nj463 <niju0026@gmail.com>
2022-06-11 16:43:27 +0530

    Added-Big-Timer
    Level TImer Added

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -6,19 +6,33 @@ using UnityEngine.UI;
 public class Timer : MonoBehaviour
 {
     public GameObject[] player;
-    public float timetochange = 5;
+    public float timetochange = 5f;
+    private float timeInMinute = 5f;
+    private float timeinSeconds ;
+    private float secondsInMinute = 59f;
     private float starttime;
     public Text timetext;
+    public Text leveltimetext;
 
     // Start is called before the first frame update
     void Start()
     {
         starttime = timetochange;
+        timeinSeconds = secondsInMinute;
     }
 
     // Update is called once per frame
     void Update()
     {
+        timeinSeconds -=Time.deltaTime;
+        if (timeinSeconds <= 0)
+        {
+            timeInMinute -= 1;
+            timeinSeconds = secondsInMinute;
+
+        }
+        leveltimetext.text = (Mathf.Round (timeInMinute) + ":" + Mathf.Round(timeinSeconds)).ToString();
+
         //To show the time on screen 
         timetext.text =Mathf.Round (starttime).ToString();
         
@@ -27,24 +41,32 @@ public class Timer : MonoBehaviour
         //Check if the timer has reached to zero
         if(starttime<=0)
         {
-            //Check which player is active and then disable that active one and enable the other one
-            if(player[0].activeInHierarchy)
-            {
-                player[0].SetActive(false);
-                player[1].SetActive(true);
 
-            }
-            else if (player[1].activeInHierarchy)
-            {
-                player[1].SetActive(false);
-                player[0].SetActive(true);
-
-            }
-            //Reset the timer
-            starttime = timetochange;
-
+            ChangePlayer();
 
         }
+
+    }
+
+    
+
+    void ChangePlayer()
+    {
+        //Check which player is active and then disable that active one and enable the other one
+        if (player[0].activeInHierarchy)
+        {
+            player[0].SetActive(false);
+            player[1].SetActive(true);
+
+        }
+        else if (player[1].activeInHierarchy)
+        {
+            player[1].SetActive(false);
+            player[0].SetActive(true);
+
+        }
+        //Reset the timer
+        starttime = timetochange;
     }
     
 }
