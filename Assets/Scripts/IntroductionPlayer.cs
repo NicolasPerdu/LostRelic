90e00d8f2a68ae3234eb574ea099be5c18508c97
ochiogrande <andreicontact@yahoo.com>
2022-06-14 12:53:51 +0100

    Intro Scene is in work

---
+++ Assets/Scripts/IntroductionPlayer.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroductionPlayer : PhysicsObject
+{
+    [SerializeField] private float horizontalSpeed;
+    [SerializeField] private float verticalSpeed;
+    [SerializeField] private int attackPower;
+
+    [SerializeField] private float attackDuration;
+    [SerializeField] private GameObject attackBox;
+
+    public GameObject AttackBox { get => attackBox; set => attackBox = value; }
+    public int AttackPower { get => attackPower; set => attackPower = value; }
+
+    //used to enable double Jump
+    private bool canDoubleJump;
+
+
+
+    //Use to disable the movement, Currently player 2 is disabled
+    public bool canMove;
+
+    //To use the method of cameracontrol script
+    private CameraControll camControl;
+
+
+
+    //singleton instantiation 
+    private static Player2 instance;
+    public static Player2 Instance
+    {
+        get
+        {
+            if (instance == null) instance = GameObject.FindObjectOfType<Player2>();
+            return instance;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //To get the CameraControl script and assign our name to it
+        camControl = FindObjectOfType<CameraControll>();
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (canMove)
+        {
+            targetVelocity = new Vector2(Input.GetAxis("Horizontal") * horizontalSpeed, 0);
+            if (targetVelocity.x < -0.1)
+            {
+                transform.localScale = new Vector3(-1, transform.localScale.y);
+            }
+            else if (targetVelocity.x > 0.1)
+            {
+                transform.localScale = new Vector3(1, transform.localScale.y);
+            }
+            if (Input.GetButtonDown("Jump"))
+            {
+                Jump();
+            }
+            if (Input.GetButtonDown("Fire1"))
+            {
+                StartCoroutine(ActivateAttack());
+            }
+
+            //If canMove is true than pass this player as argument to the UpdatePlayer function in cameracontrol script
+            camControl.UpdatePlayer(this.gameObject);
+
+        }
+
+    }
+    private void Jump()
+    {
+        if (grounded)
+        {
+            velocity.y = verticalSpeed;
+            canDoubleJump = true;
+        }
+        else
+        {
+            if (canDoubleJump)
+            {
+                canDoubleJump = false;
+                velocity.y = verticalSpeed;
+            }
+        }
+
+    }
+
+
+
+    private IEnumerator ActivateAttack()
+    {
+        attackBox.SetActive(true);
+        yield return new WaitForSeconds(attackDuration);
+        attackBox.SetActive(false);
+    }
+
+    //Public function that will determine whether canmove is true or not via the passed in parameter
+    public void ActiveInScene(bool swapPlayer)
+    {
+        //As canMove is false in the start so it will be true when swapplayer is true and next  time it will false
+        canMove = swapPlayer;
+
+    }
+}
